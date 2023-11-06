3d15290bfdf030040d815e06715feb7cf125cf9f
yeonhee <gopqop1344@naver.com>
2023-11-06 11:45:24 +0900

    1106_1145

--- plan-project-develop/src/main/java/com/spring/market_1/schedule/controller/ScheduleController.java
+++ plan-project-develop/src/main/java/com/spring/market_1/schedule/controller/ScheduleController.java
@@ -2,13 +2,14 @@ package com.spring.market_1.schedule.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.spring.market_1.ObjectController;
-
+@Controller("ScheduleController")
 public class ScheduleController implements ObjectController{
 
 	@Override
@@ -60,22 +61,24 @@ public class ScheduleController implements ObjectController{
 	}
 	
 	@RequestMapping(value = {"/schedule/Myschedule.do",
-},method = RequestMethod.GET)
-public ModelAndView Form(@RequestParam(value = "type", required = false) String type, HttpServletRequest request) throws Exception {
-	ModelAndView mav = new ModelAndView();
-
-	String uri = request.getRequestURI();
-	System.out.println(uri); ///plan/schedule/Myschedule.do 
+		},method = RequestMethod.GET)
+	public ModelAndView Form(@RequestParam(value = "type", required = false) String type, HttpServletRequest request) throws Exception {
+		ModelAndView mav = new ModelAndView();
 	
-
-	if(uri.endsWith("Myschedule.do")) {
-		mav.setViewName("/schedule/Myschedule");
+		String uri = request.getRequestURI();
+		System.out.println(uri); ///plan/schedule/Myschedule.do 
 		
-	}else if(uri.endsWith("LoginForm.do")) {
-		mav.setViewName("/member/LoginForm");
+	
+		if(uri.endsWith("Myschedule.do")) {
+			mav.setViewName("/schedule/Myschedule");
+			
+		}else if(uri.endsWith("LoginForm.do")) {
+			mav.setViewName("/member/LoginForm");
+		}
+		
+		
+		return mav;
 	}
 	
 	
-	return mav;
-}
 }
