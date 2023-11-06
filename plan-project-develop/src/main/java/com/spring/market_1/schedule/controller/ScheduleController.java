4fad34aef964a943483a4c682ca06643c68aed36
daon <reurireury@gmail.com>
2023-11-06 09:40:40 +0900

    수정1106

--- plan-project-develop/src/main/java/com/spring/market_1/schedule/controller/ScheduleController.java
+++ plan-project-develop/src/main/java/com/spring/market_1/schedule/controller/ScheduleController.java
@@ -2,13 +2,14 @@ package com.spring.market_1.schedule.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.spring.market_1.ObjectController;
-
+@Controller("ScheduleController")
 public class ScheduleController implements ObjectController{
 
 	@Override
